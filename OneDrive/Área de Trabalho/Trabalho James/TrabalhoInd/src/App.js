1f49a60ba1b700d7ab888d0743bf0497ee8b6f51
BrunooKuster <131623696+BrunooKuster@users.noreply.github.com>
2023-12-11 22:05:19 -0300

    Primeiro commit

---
+++ OneDrive/Área de Trabalho/Trabalho James/TrabalhoInd/src/App.js
@@ -0,0 +1,26 @@
+import CadastroCurso from "./pages/CadastroCurso.js";
+import CadastroDePeriodo from "./pages/CadastroDePeriodo.js";
+import CadastroProfessor from "./pages/CadastroProfessor.js";
+import CadastroDeSalas from "./pages/CadastroDeSalas.js";
+import CadastroDeDesafio from "./pages/CadastroDeDesafio.js";
+import CalendarioDeHorarios from "./pages/CalendarioDeHorarios.js";
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+      <CalendarioDeHorarios/>
+      <CadastroCurso/>
+      <CadastroDePeriodo/>
+      <CadastroProfessor/>
+      <CadastroDeSalas/>
+      <CadastroDeDesafio/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
